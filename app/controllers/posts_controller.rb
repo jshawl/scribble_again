efe45840d78d0afc3b9d1292c794df900d9c6d92
Thom Householder <thomhouseholder@gmail.com>
2015-11-15 18:46:36 -0500

    posts CRUD complete

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create!(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
+    end
+  end
+
+
+  def update
+    @post = Post.find(params[:id])
+    @post = Post.update(post_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+
+private
+  def post_params
+    params.require(:post).permit(:title, :topic, :body)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115215535) do
+ActiveRecord::Schema.define(version: 20151115220342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
