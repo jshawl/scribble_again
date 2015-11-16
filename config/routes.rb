678807538a002dba3449a3de87d93fcce243e13a
Thom Householder <thomhouseholder@gmail.com>
2015-11-15 20:16:37 -0500

    stuck trying to create new comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
     root 'posts#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-    
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-    resources :posts
+    resources :posts do
+      resources :comments
+    end
   # Example resource route with options:
   #   resources :products do
   #     member do
